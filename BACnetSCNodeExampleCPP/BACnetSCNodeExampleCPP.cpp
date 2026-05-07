352b34009074906a591fb176315b173f954857c8
Alex Fontaine <afontaine@chipkin.com>
2026-05-07 09:43:37 -0700

    Add initial BACnet SC Node C++ example project
    This commit establishes the foundational project structure for a C++ example demonstrating a BACnet SC Node.
    
    It includes:
    - Visual Studio solution and project files (`.sln`, `.vcxproj`) for the `BACnetSCNodeExampleCPP` application.
    - A comprehensive `.gitignore` file to exclude common development artifacts (Visual Studio, build outputs, CMake, Windows files, and submodule contents).
    - A `submodules/readme.md` to provide instructions on setting up the CAS BACnet Stack submodule.

---
+++ BACnetSCNodeExampleCPP/BACnetSCNodeExampleCPP.cpp
@@ -0,0 +1,23 @@
+/*
+ * BACnet SC Node Example C++
+ * ----------------------------------------------------------------------------
+ * BACnetSCNodeExample.cpp
+ *
+ * In this CAS BACnet Stack example, we create a simple BACnet SC node with various
+ * objects and properties from an example database.
+ *
+ * More information https://github.com/chipkin/BACnetSCNodeExampleCPP
+ *
+ * This file contains the 'main' function. Program execution begins and ends there.
+ *
+ * Created by: Alex Fontaine
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
